c3fdf277fe9f3602825a61b8417860a27d29d0fb
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-03-27 22:48:51 -0600

    `Added API documentation and coreapi to project`

--- api/models.py
+++ api/models.py
@@ -8,9 +8,11 @@ STATUS_CHOICES = (
     ('R', 'Resuelto'),
 )
 
+
 class Incident (models.Model):
     incident_id = models.AutoField(primary_key=True)
     reporter = models.CharField(max_length=255)
     description = models.TextField()
-    status = models.CharField(max_length=255,choices=STATUS_CHOICES, default="Pending")
+    status = models.CharField(
+        max_length=255, choices=STATUS_CHOICES, default="Pending")
     created_at = models.DateTimeField(auto_now_add=True)
--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,5 @@ router.register(r'incidents', views.IncidentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
