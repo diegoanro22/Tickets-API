e049a03ebc83fd2628a80b59f5118f3ac4b406ee
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-04-10 12:30:01 -0600

    `Added template directory and CSS styles, removed index.html`

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    // Contenedor principal
+    const container = document.createElement('div');
+    container.id = "main-container";
+
+    document.body.appendChild(container);
+
+    // Iniciar vista de lista
+    // renderListView();
+});
