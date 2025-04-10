ce9d98482c12ee2353918a1e801096011a6b3b91
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-04-10 03:47:25 -0600

    `Added static files directory and index view to project settings and URLs.`

--- Tickets/urls.py
+++ Tickets/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('docs/', include_docs_urls(title='API Documentation')),
+    path('', TemplateView.as_view(template_name="index.html"))
 ]
