771ab168d24ae2db7fdb8ef55b8d1ac9aa55263d
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-04-23 02:13:14 -0600

    `Update .env.example and docker-compose.yml to remove .env volume mount and update SECRET_KEY. Update wait-for-db.sh to generate SECRET_KEY if not present.`

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -9,14 +9,6 @@ done
 
 echo "PostgreSQL está listo, aplicando migraciones..."
 
-# Genera una SECRET_KEY si no existe
-if ! grep -q "SECRET_KEY=" .env; then
-  echo "Generando SECRET_KEY aleatoria..."
-  SECRET_KEY=$(python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
-  echo "SECRET_KEY='$SECRET_KEY'" >> .env
-  echo "SECRET_KEY generada y añadida al .env."
-fi
-
 # Aplica migraciones
 python manage.py migrate
 
